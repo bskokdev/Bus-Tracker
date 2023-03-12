55b0cb79fc3dc53d7b4c11909aea488f2691590f
Boris Skok <skokcmd@gmail.com>
2023-03-12 20:11:05 +0100

    updated structs for the data needs

--- domain/domain.go
+++ domain/domain.go
@@ -1,10 +1,13 @@
 package domain
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
+// Struct to store the entire bus telemetry topic
 type BusTelemetry struct {
 	gorm.Model
 	ID            uuid.UUID
@@ -21,22 +24,49 @@ type BusTelemetry struct {
 	Headsign      string
 	StartTime     string
 	NextStop      int
-	GeohashLevel  int
-	Geohash       string
-	Sid           int
+	Lat           float64
+	Lon           float64
+}
+
+// Struct to store the bus telemetry payload
+type BusTelemetryPayload struct {
+	Vp struct {
+		Desi  string    `json:"desi"`
+		Dir   string    `json:"dir"`
+		Oper  int       `json:"oper"`
+		Veh   int       `json:"veh"`
+		Tst   time.Time `json:"tst"`
+		Tsi   int       `json:"tsi"`
+		Spd   float64   `json:"spd"`
+		Hdg   int       `json:"hdg"`
+		Lat   float64   `json:"lat"`
+		Lon   float64   `json:"long"`
+		Acc   float64   `json:"acc"`
+		Dl    int       `json:"dl"`
+		Odo   int       `json:"odo"`
+		Drst  int       `json:"drst"`
+		Oday  string    `json:"oday"`
+		Jrn   int       `json:"jrn"`
+		Line  int       `json:"line"`
+		Start string    `json:"start"`
+		Loc   string    `json:"loc"`
+		Stop  any       `json:"stop"`
+		Route string    `json:"route"`
+		Occu  int       `json:"occu"`
+	} `json:"VP"`
 }
 
 // BusTelemetry DTO for API
 type BusDTO struct {
-	Id                  uuid.UUID
-	Number              int
-	Lon                 float64
-	Lat                 float64
-	NextStop            string
-	NextStopArrivalTime string
+	Id       uuid.UUID
+	RouteId  int
+	HeadSign string
+	NextStop string
+	Lat      float64
+	Lon      float64
 }
 
-func NewBus(number int, lon, lat float64, nextStop, nextStopArrivalTime string) *BusDTO {
+func NewBusDTO(number int, lon, lat float64, nextStop, nextStopArrivalTime string) *BusDTO {
 	return &BusDTO{
 		Id:                  uuid.New(),
 		Number:              number,
