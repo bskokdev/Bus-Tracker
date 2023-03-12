7257719350d5edbac8bea4e6df4288657b00738d
Boris Skok <skokcmd@gmail.com>
2023-03-12 20:13:10 +0100

    exposed simple http route to get telemetries (limit 10)

--- api/httpServer.go
+++ api/httpServer.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"main/domain"
 	"net/http"
 
 	"gorm.io/gorm"
@@ -28,11 +30,35 @@ func (s *Server) Start() error {
 	log.Println("Starting HTTP server on port " + s.address)
 	// Routes
 	http.HandleFunc("/api/v1/buses/nearest", handleGetNearestBuses(s.db))
+	http.HandleFunc("/api/v1/telemetries", handleGetAllTelemetries(s.db))
+
 	return http.ListenAndServe(s.address, nil)
 }
 
+// Function to handle GET requests to /api/v1/telemetries
+// Returns all telemetries from the database
+func handleGetAllTelemetries(db *gorm.DB) Handler {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// TODO: add pagination
+		var telemetries []domain.BusTelemetry
+		res := db.Limit(10).Find(&telemetries)
+		if res.Error != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(res.Error.Error()))
+			return
+		}
+		jsonData, err := json.Marshal(telemetries)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonData)
+	}
+}
+
 func handleGetNearestBuses(db *gorm.DB) Handler {
-	// TODO: query the database for the closest buses and save in a variable (slice)
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("List of closest buses"))
 	}
