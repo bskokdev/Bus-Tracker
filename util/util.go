a1a802dbf12332f070aaa0cba807ea50c9b23aa8
Boris Skok <skokcmd@gmail.com>
2023-03-13 23:21:39 +0100

    added page offset util function for pagination

---
+++ util/util.go
@@ -0,0 +1,11 @@
+package util
+
+func GetDistanceInKm(lat1, lon1, lat2, lon2 float64) float64 {
+	// TODO: implement this function
+
+	return 0
+}
+
+func GetPageOffset(page, pageSize int) int {
+	return (page - 1) * pageSize
+}
