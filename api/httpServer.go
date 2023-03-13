6a2a92b565ce79252a08f35db94eaea960428348
Boris Skok <skokcmd@gmail.com>
2023-03-13 23:22:24 +0100

    using offset util func & naive nearest buses impl

--- api/httpServer.go
+++ api/httpServer.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 	"log"
 	"main/domain"
+	"main/parser"
+	"main/util"
 	"net/http"
 	"strconv"
 
 	"gorm.io/gorm"
+)
+
+// default values for pagination
+var (
+	pageSize int = 10
+	page     int = 1
 )
 
 type Server struct {
@@ -29,7 +37,7 @@ func NewServer(address string, db *gorm.DB) *Server {
 // Start starts the HTTP server and listens for requests
 func (s *Server) Start() error {
 	log.Println("Starting HTTP server on port " + s.address)
-	// Routes
+	// Define routes
 	http.HandleFunc("/api/v1/buses/nearest", handleGetNearestBuses(s.db))
 	http.HandleFunc("/api/v1/telemetries", handleGetAllTelemetries(s.db))
 
@@ -42,9 +50,6 @@ func (s *Server) Start() error {
 // example: http://{host}:{port}/api/v1/telemetries?page=1&pageSize=10
 func handleGetAllTelemetries(db *gorm.DB) Handler {
 	return func(w http.ResponseWriter, r *http.Request) {
-		pageSize := 10
-		page := 1
-
 		// Get the page number from the query parameters
 		pageParam := r.URL.Query().Get("page")
 		if pageParam != "" {
@@ -57,7 +62,7 @@ func handleGetAllTelemetries(db *gorm.DB) Handler {
 			pageSize, _ = strconv.Atoi(pageSizeParam)
 		}
 
-		offset := (page - 1) * pageSize
+		offset := util.GetPageOffset(page, pageSize)
 
 		var telemetries []domain.BusTelemetry
 		res := db.Limit(pageSize).Offset(offset).Find(&telemetries)
@@ -67,7 +72,7 @@ func handleGetAllTelemetries(db *gorm.DB) Handler {
 			return
 		}
 
-		// Parse to JSON
+		// Parse telemetries to JSON
 		jsonData, err := json.Marshal(telemetries)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -79,11 +84,41 @@ func handleGetAllTelemetries(db *gorm.DB) Handler {
 	}
 }
 
+// Function to handle GET requests to /api/v1/buses/nearest
+// Endpoint accepts query parameters lat and lon and page
+// pageSize is fixed via default global variable
+// Returns the 10 closest buses to the user
 func handleGetNearestBuses(db *gorm.DB) Handler {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// get lon and lat from query parameters
-		// lon := r.URL.Query().Get("lon")
+		// get lon and lat from query parameters and cast to f64
+		// this would be the user's location
+		lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
 
-		w.Write([]byte("List of closest buses"))
+		// get page of bus telemetries from the database
+		telemetries := []domain.BusTelemetry{}
+		offset := util.GetPageOffset(page, pageSize)
+		res := db.Limit(5 * pageSize).Offset(offset).Find(&telemetries)
+		if res.Error != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(res.Error.Error()))
+			return
+		}
+
+		// Parse telemetries to BusDTOs
+		buses := []domain.BusDTO{}
+		for _, telemetry := range telemetries {
+			buses = append(buses, parser.NewBusDTOFromTelemetry(telemetry, lat, lon))
+		}
+
+		// Parse buses to JSON and return
+		jsonData, err := json.Marshal(buses)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonData)
 	}
 }
