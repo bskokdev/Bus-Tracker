0c0e52181fd8a5c31695d2775b0d0343404f7b87
Boris Skok <skokcmd@gmail.com>
2023-03-15 17:51:34 +0100

    added util function to calc distance based on lat, lon

--- util/util.go
+++ util/util.go
@@ -1,11 +1,30 @@
 package util
 
-func GetDistanceInKm(lat1, lon1, lat2, lon2 float64) float64 {
-	// TODO: implement this function
+import "math"
 
-	return 0
+func GetDistanceInKm(lat1, lon1, lat2, lon2 float64) float64 {
+	radlat1 := float64(math.Pi * lat1 / 180)
+	radlat2 := float64(math.Pi * lat2 / 180)
+
+	theta := float64(lon1 - lon2)
+	radtheta := float64(math.Pi * theta / 180)
+
+	dist := math.Sin(radlat1)*math.Sin(radlat2) + math.Cos(radlat1)*math.Cos(radlat2)*math.Cos(radtheta)
+	if dist > 1 {
+		dist = 1
+	}
+
+	dist = math.Acos(dist)
+	dist = dist * 180 / math.Pi
+	dist = dist * 60 * 1.1515
+
+	dist = dist * 1.609344
+
+	return dist
 }
 
+// GetPageOffset returns the offset for the given page and page pageSize
+// Used for pagination in the database
 func GetPageOffset(page, pageSize int) int {
 	return (page - 1) * pageSize
 }
